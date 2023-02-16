60cd4c337d7af7e3f7625ce3a2f44ab2f2643cab
cksdevelop <litchenks@qq.com>
2023-02-17 01:14:17 +0800

    测试。。。hello

--- src/main/java/lambdasinaction/appa/Book.java
+++ src/main/java/lambdasinaction/appa/Book.java
@@ -10,8 +10,8 @@ public class Book {
     public static void main(String[] args) {
         Author[] authors = Book.class.getAnnotationsByType(Author.class);
         Arrays.asList(authors).stream().forEach(a -> {
+            System.out.println("Hello");
             System.out.println(a.name());
-            System.out.println("......");
         });
     }
 
