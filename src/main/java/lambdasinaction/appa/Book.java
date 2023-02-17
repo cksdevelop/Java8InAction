f4d8928be4cab4f713c82f186bdde2dd810f3a7c
cksdevelop <litchenks@qq.com>
2023-02-17 12:36:41 +0800

    还原....

--- src/main/java/lambdasinaction/appa/Book.java
+++ src/main/java/lambdasinaction/appa/Book.java
@@ -10,9 +10,7 @@ public class Book {
     public static void main(String[] args) {
         Author[] authors = Book.class.getAnnotationsByType(Author.class);
         Arrays.asList(authors).stream().forEach(a -> {
-            System.out.println("Hello");
             System.out.println(a.name());
-            System.out.println("TEST001");
         });
     }
 
